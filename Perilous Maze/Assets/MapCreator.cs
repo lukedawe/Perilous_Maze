4ab082fd5fc2933a8863ba3512433a67ec7d657a
lukedawe <luke.dawe@gmail.com>
2021-02-22 16:42:04 +0000

    Made a start on making the path for the maze.

--- Perilous Maze/Assets/MapCreator.cs
+++ Perilous Maze/Assets/MapCreator.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 public class MapCreator : MonoBehaviour
 {
+    public GameObject straightHedge;
+    public GameObject hedgeTurn;
+    public GameObject hedgeWallCorner;
+    public GameObject straightHedgeWall;
+
     public int mapSize;
-    public List<Vector3Int> route;
+    private List<Vector3Int> route;
     public void AddLine(Vector3Int line)
     {
         route.Add(line);
@@ -18,41 +23,32 @@ public class MapCreator : MonoBehaviour
 
     public void mapStart()
     {
-        bool endFound = false;
-        char[] axis = { 'x', 'z' };
 
-        // make the plane ofr the map
+        // make the plane for the map
         GameObject map = this.CreatePlane(new Vector3Int(this.mapSize, 0, 0));
         map.transform.localScale = new Vector3Int((this.mapSize / 5), 1, (this.mapSize / 5));
 
         // make a start for the maze and add it to the route
-        Vector3Int start = new Vector3Int(0, 1, Random.Range(-mapSize, mapSize));
-        route.Add(start);
-
-        // add a cube to the starting position
-        this.CreateCube(start);
+        int startZCoord = Random.Range(-mapSize+1, mapSize-1);
+        Vector3 start = new Vector3(1, 0.5f, startZCoord);
+        // add a wall to the starting position
+        this.CreateStraightHedge(start);
+        
+        Vector3 next = new Vector3(2, 0.5f, startZCoord);
+        // route.Add(start);
+        RouteFinder(next);
+        
 
         // now we have the start, we always want to head straight into the maze
-        Vector3Int entryPoint = new Vector3Int(-1, 0, 0);
-        route.Add(entryPoint);
+        // Vector3Int entryPoint = new Vector3Int(-1, 0, 0);
+        // route.Add(entryPoint);
+        
+    }
 
-
-
-        // now we have the starting point of the route, we can head in a random direction
-        //while (!endFound)
-        //{
-        //    Vector3Int newLine = new Vector3Int();
-        //    char changedAxis = axis[Random.Range(0, 1)];
-        //    switch (changedAxis)
-        //    {
-        //        case 'x':
-        //            newLine.x = Random.Range(-10, 10);
-        //            break;
-        //        case 'z':
-        //            newLine.z = Random.Range(-10, 10);
-        //            break;
-        //    }
-        //}
+    public void CreateStraightHedge(Vector3 position)
+    {
+        // make a cube for testing purposes
+        Instantiate(this.straightHedge, position, Quaternion.identity);
     }
 
     public void CreateCube(Vector3Int position)
@@ -70,8 +66,16 @@ public class MapCreator : MonoBehaviour
         return plane;
     }
 
-    public void createHedge(Vector3Int position)
+    public void RouteFinder(Vector3 start)
     {
-        return;
+        // we are starting our journey between -19 and 19.
+        bool endFound = false;
+        int currentAngle = 0;
+        // we need to decide which way we are going to send the player.
+        while (!endFound)
+        {
+            // we have a 40/40 plane in which to make the path
+            // we have a hedge that goes 2 blocks forward OR one that goes 6 forward and 5 right.
+        }
     }
 }
